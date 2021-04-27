7b03a0dcd32d414574684706fe3e4a2cd7c85a2e
Claes Ö <>
2021-04-27 10:10:01 +0200

    1.1.5

---
+++ PluginApiSimple_one.php
@@ -0,0 +1,141 @@
+<?php
+class PluginApiSimple_one{
+  public function __call($method, $args) {
+    /**
+     * 
+     */
+    if(wfRequest::get('xurl')){
+      header('Access-Control-Allow-Origin: '.wfRequest::get('xurl'));
+    }else{
+      header('Access-Control-Allow-Origin: *');
+    }
+    header('Access-Control-Allow-Credentials: true');
+    /**
+     * 
+     */
+    $method = substr($method, 5);
+    $class = wfGlobals::get('class');
+    $settings = new PluginWfArray(wfGlobals::get("settings/plugin_modules/$class/settings"));
+    $key = wfRequest::get('key');
+    $remote_addr = wfServer::getRemoteAddr();
+    $json = new PluginWfArray(array('theme' => $this->get_theme(), 'request' => array('xurl' => wfRequest::get('xurl'), 'url' => wfServer::calcUrl(), 'class' => $class, 'method' => $method, 'key' => $key, 'remote_addr' => $remote_addr), 'user' => array('role' => array()), 'error' => array(), 'data' => array()));
+    $this_class = __CLASS__;
+    /**
+     * Security key.
+     */
+    $security_keys_data = array();
+    if($settings->get('security/keys')){
+      $key_match = false;
+      foreach($settings->get('security/keys') as $value){
+        $v = new PluginWfArray($value);
+        if($v->get('value')==$key){
+          $key_match = true;
+          $security_keys_data = $v->get('data');
+          break;
+        }
+      }
+      if(!$key_match){
+        $json->set('error/', array('message' => "$this_class says: Param security/keys does not match key."));
+      }
+    }
+    $settings->set("methods/$method/security_keys_data", $security_keys_data);
+    /**
+     * Security remote_addr.
+     */
+    if($settings->get('security/remote_addr')){
+      $remote_addr_match = false;
+      foreach($settings->get('security/remote_addr') as $value){
+        $v = new PluginWfArray($value);
+        if($v->get('value')==$remote_addr){
+          $remote_addr_match = true;
+          break;
+        }
+      }
+      if(!$remote_addr_match){
+        $json->set('error/', array('message' => "$this_class says: Param security/remote_addr does not match remote_addr."));
+      }
+    }
+    /**
+     * Method.
+     */
+    if(!sizeof($json->get('error'))){
+      if($settings->get("methods/$method")){
+        $plugin = $settings->get("methods/$method/plugin");
+        wfPlugin::includeonce($plugin);
+        $obj = wfSettings::getPluginObj($plugin);
+        $obj_method = $settings->get("methods/$method/name");
+        if(!method_exists($obj, $obj_method) && !method_exists($obj, '__call')){
+          $json->set('error/message/', "$this_class says: Method $obj_method does not exist in plugin $plugin.");
+        }else{
+          $method_data = new PluginWfArray($obj->$obj_method($settings->get("methods/$method")));
+          $json->set('data', $method_data->get('data'));
+          /**
+           * error
+           */
+          if($method_data->get('error')){
+            foreach($method_data->get('error') as $v){
+              $json->set('error/', $v);
+            }
+          }
+        }
+      }else{
+        $json->set('error/message/', "$this_class says: Param methods does not has method $method.");
+      }
+    }
+    /**
+     * Slack
+     */
+    if($settings->get('slack')){
+      $run = true;
+      /**
+       * 
+       */
+      if($settings->get('slack/domain_filter') && !strstr(wfServer::getServerName(), $settings->get('slack/domain_filter'))){
+        $run = false;
+      }
+      /**
+       * 
+       */
+      if($run){
+        /**
+         * Set data in text.
+         */
+        $settings->set('slack/text/remote_addr', wfServer::getRemoteAddr().'');
+        $settings->set('slack/text/request_uri', wfServer::getServerName().wfServer::getRequestUri() );
+        $settings->set('slack/text/error', $json->get('error'));
+        if(false){
+          $settings->set('slack/text/json', $json->get());
+          $settings->set('slack/text/session', wfUser::getSession()->get());
+        }
+        /**
+         * Send.
+         */
+        wfPlugin::includeonce('slack/webhook_v1');
+        $slack_webhook = new PluginSlackWebhook_v1();
+        $slack_webhook->url = $settings->get('slack/webhook');
+        $slack_webhook->channel = $settings->get('slack/group');
+        $slack_webhook->text = wfHelp::getYmlDump($settings->get('slack/text'), true);
+        $slack_webhook->send();
+      }
+    }
+    /**
+     * 
+     */
+    $json->set('user/role/client', wfUser::hasRole('client'));
+    /**
+     * 
+     */
+    if(wfRequest::get('output')=='yml'){
+      echo '<pre>';
+      echo wfHelp::getYmlDump($json->get());
+      echo '</pre>';
+      exit;
+    }else{
+      exit(json_encode($json->get()));
+    }
+  }
+  private function get_theme(){
+    $theme_manifest = new PluginWfYml('/theme/[theme]/config/manifest.yml');
+    return array('version' => $theme_manifest->get('version'));
+  }
+}
---
+++ README.md
@@ -0,0 +1,89 @@
+# Buto-Plugin-ApiSimple_one
+This is a simple API to retrieve information.
+
+## Settings
+```
+plugin_modules:
+  api_v1:
+    plugin: api/simple_one
+    settings:
+      slack:
+        webhook: _url_
+        group: _group_
+        domain_filter: www.world.com
+      security:
+        keys:
+          -
+            value: abcdef
+            data:
+              some_id: 1234
+        _remote_addr:
+          -
+            value: '127.0.0.1'
+      methods:
+        users:
+          plugin: some/plugin
+          name: users
+```
+### slack
+Optional to alert on Slack. Use param domain_filter to only send if on a specific domain.
+
+### security/keys
+Optional to verify user by a key. One could use data parameter to assosiate key with values.
+
+### security/remote_addr
+Optional to verify user by ip.
+
+### methods
+Here we send users to plugin some/plugin and method users along with some data.
+
+## Url
+```
+/api_v1/users?key=abcdef
+```
+
+## Result
+```
+user:
+  role:
+    client: false
+error: {  }
+data:
+  error: {  }
+  rs:
+    - James
+    - Jane
+```
+
+### user
+If param user/role/client is true user has sign in. 
+
+### error
+Param error is for error handling.
+
+### data
+This has value of the method involved in request.
+
+## Output
+Set output=yml to render yml instead of json.
+```
+/api_v1/users?key=abcdef&output=yml
+```
+
+## Method
+Method example.
+```
+public function users($json, $settings, $key_data){
+  $data = new PluginWfArray();
+  $data->set('error', array());
+  $data->set('rs', array('James', 'Jane'));
+  return $data->get();
+}
+```
+
+## Access-Control-Allow-Origin
+Set param xurl to client url to allow requests between different domains. 
+In example below we call domain world.com from localhost. 
+```
+http://world.com?xurl=http://localhost
+```
